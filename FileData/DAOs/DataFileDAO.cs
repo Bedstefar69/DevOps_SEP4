42c69f859a260fe76ad53535599ae9a2023cef40
Bedstefar69 <andreastamsen@gmail.com>
2023-04-26 14:25:35 +0200

    Save Data to file

---
+++ FileData/DAOs/DataFileDAO.cs
@@ -0,0 +1,39 @@
+﻿using Application.DAO_Interfaces;
+using Domain.Models;
+
+namespace FileData.DAOs;
+
+public class DataFileDAO : IDataDAO
+{
+
+    private readonly FileContext context;
+    
+    public DataFileDAO(FileContext context)
+    {
+        this.context = context;
+    }
+    public Task<Data> CreateAsync(Data data)
+    {
+        int dataID = 1;
+        if (context.data.Any())
+        {
+            dataID = context.data.Max(u => u.Id);
+            dataID++;
+        }
+
+        data.Id = dataID;
+
+        context.data.Add(data);
+        context.SaveChanges();
+
+        return Task.FromResult(data);
+    }
+
+    public Task<Data?> GetByID(int id)
+    {
+        Data? existing = context.data.FirstOrDefault(u =>
+            u.Id ==id
+        );
+        return Task.FromResult(existing);
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,9 @@
+using Application.DAO_Interfaces;
+using Application.Logic;
+using Application.Logic_Interfaces;
+using FileData;
+using FileData.DAOs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<FileContext>();
+builder.Services.AddScoped<IDataDAO, DataFileDAO>();
+builder.Services.AddScoped<IDataLogic, DataLogic>();
 
 var app = builder.Build();
 
