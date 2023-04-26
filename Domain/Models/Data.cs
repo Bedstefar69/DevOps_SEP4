95751bb75b918ff4aa7563c2a6f13563d6b6237d
Bedstefar69 <andreastamsen@gmail.com>
2023-04-26 13:27:25 +0200

    Domain + Data Model

---
+++ Domain/Models/Data.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models;
+
+public class Data
+{
+    public int Id { get; set; }
+    private string Body { get; set; }
+
+    public Data(int id, string body)
+    {
+        Id = id;
+        Body = body;
+        
+    }
+}
