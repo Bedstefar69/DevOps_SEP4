970b57c9a100e9d3f90c6d74171eba6b029d1aa8
EKKA_GITHUB <48787232+DeadlyPoizon@users.noreply.github.com>
2023-04-26 13:58:24 +0200

    v0.1 WebSocket - Christian

---
+++ WebSocket/Client.cs
@@ -0,0 +1,28 @@
+﻿using WebSocketSharp;
+
+namespace WebSocket
+{
+
+   public class Client
+    {
+        public void init()
+        {
+            using (WebSocketSharp.WebSocket webSocket = new WebSocketSharp.WebSocket("ws://127.0.0.1:1337/Echo"))
+            {
+                webSocket.OnMessage += Socket_OnMessage;
+                
+                webSocket.Connect();
+                webSocket.Send("Hi");
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void Socket_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved: " + e.Data);
+        }
+
+    }
+
+}
---
+++ WebSocket/Server.cs
@@ -0,0 +1,37 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocket
+{
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved Message: " + e.Data);
+            Send(e.Data);
+        }
+    }
+
+    public class EchoAll : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved from all: " + e.Data);
+            Sessions.Broadcast(e.Data);
+        }
+    }
+
+    public class Server
+    {
+        
+        public void init()
+        {
+            WebSocketServer socketServer = new WebSocketServer("ws://127.0.0.1:1337");
+            socketServer.AddWebSocketService<Echo>("/Echo");
+            socketServer.AddWebSocketService<EchoAll>("/EchoAll");
+            
+            socketServer.Start();
+            
+        }
+    }
+}
