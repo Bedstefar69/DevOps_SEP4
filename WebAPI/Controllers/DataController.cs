06d855cefb273ea98c167ce379c870cb731546bf
Bedstefar69 <andreastamsen@gmail.com>
2023-04-26 14:10:22 +0200

    API Controller Endpoint to create data

---
+++ WebAPI/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Logic_Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DataController : ControllerBase
+{
+    private readonly IDataLogic dataLogic;
+
+    public DataController(IDataLogic dataLogic)
+    {
+        this.dataLogic = dataLogic;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<Data>> CreateAsync(Data dto)
+    {
+        try
+        {
+            Data data = await dataLogic.CreateAsync(dto);
+            return Created($"/data/{data.Id}", data);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
