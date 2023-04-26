38d83bed247c1e8f9157b6b41a01b5ca15a02265
EKKA_GITHUB <48787232+DeadlyPoizon@users.noreply.github.com>
2023-04-26 13:58:24 +0200

    v0.1 WebSocket - Christian

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using WebSocketSharp;
+
+namespace WebSocket
+{
+
+   public class Client
+    {
+        public static void Main()
+        {
+            using (WebSocketSharp.WebSocket webSocket = new WebSocketSharp.WebSocket("ws://127.0.0.1:4242/Echo"))
+            {
+                webSocket.OnMessage += Socket_OnMessage;
+                
+                webSocket.Connect();
+                webSocket.Send("Hi");
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void Socket_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved: " + e.Data);
+        }
+
+    }
+
+}
