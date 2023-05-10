6e079705e69904cbaeeda1daf561249e599ab346
EKKA_GITHUB <48787232+DeadlyPoizon@users.noreply.github.com>
2023-05-10 13:21:21 +0200

    10-05-2023 - WebSocket oppe og kører -> ingen funktionalitet endnu

---
+++ WebSocketJava/src/main/java/org/example/main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class main {
+    public static void main(String[] args) {
+
+        WebsocketClient websocketClient = new WebsocketClient("wss://iotnet.teracom.dk/app?token=vnoVQQAAABFpb3RuZXQudGVyYWNvbS5ka44TEFZ6iw5hEImHN64AWw0=");
+
+    }
+}
